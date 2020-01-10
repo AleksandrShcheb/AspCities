72ec6bd4ea9eca4d37efe138a1eaa74d5dbe3913
Aleksandr <danserdgit@yandex.ru>
2020-01-10 11:46:00 +0300

    Example Asp project

---
+++ CitiesController/Controllers/CitiesController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CitiesController.DataStore;
+using CitiesController.Models;
+
+namespace CitiesController.Controllers
+{
+    public class CitiesController : Controller
+    {
+        [HttpGet("/api/cities")]
+        public IActionResult GetCities()
+        {
+            var citiesDataStore = CitiesDataStore.GetInstance();
+            var cities = citiesDataStore.Cities;
+
+            return Ok(cities);
+
+        }
+        [HttpGet("/api/cities/{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var citiesDataStore = CitiesDataStore.GetInstance();
+            foreach (var city in citiesDataStore.Cities)
+            {
+                if (city.Id == id)
+                {
+                    return Ok(city);
+                }
+            }
+
+            return NotFound();
+        }
+        [HttpPost("/api/cities/")]
+        public IActionResult AddCity([FromBody] City city)
+        {
+            var citiesDataStore = CitiesDataStore.GetInstance();
+            citiesDataStore.Cities.Add(city);
+
+            return Created("/api/cities/" + city.Id, city);
+        }
+
+
+        [HttpDelete("/api/cities/{id}")]
+        public IActionResult DeleteCity(int id)
+        {
+            var citiesDataStore = CitiesDataStore.GetInstance();
+            foreach (var city in citiesDataStore.Cities)
+            {
+                if (city.Id == id)
+                {
+                    citiesDataStore.Cities.Remove(city);
+                    return NotFound();
+                }
+            }
+            return Ok(citiesDataStore.Cities);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ CitiesController/DataStore/CitiesDataStore.cs
@@ -0,0 +1,39 @@
+﻿using CitiesController.Models;
+using System.Collections.Generic;
+
+namespace CitiesController.DataStore
+{
+    public class CitiesDataStore
+    {
+        private static CitiesDataStore _citiesDataStore;
+
+        public List<City> Cities { get; }
+        private CitiesDataStore()
+        {
+            Cities = new List<City> 
+            {
+                new City
+                {
+                    Id = 1,
+                    Name = "Moscow",
+                    Description = "It is the capital of Russia"
+                },
+                 new City
+                {
+                    Id = 2,
+                    Name = "Sochi",
+                    Description = "It's hot in here"
+                }
+            };
+        }
+
+        public static CitiesDataStore GetInstance()
+        {
+            if (_citiesDataStore == null)
+            {
+                _citiesDataStore = new CitiesDataStore();
+            }
+            return _citiesDataStore;
+        }
+    }
+}
